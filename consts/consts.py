2fa1ee7661da59e8c4ba2d1b81fb1223d7cd6bf6
The Rabak <arielvaron@gmail.com>
2021-05-17 14:41:23 +0300

    testing first commit

---
+++ consts/consts.py
@@ -0,0 +1,8 @@
+model_name = "resnet101"
+optimizer_name = "AdamW"
+data_dir = 'assets/flowers'
+train_dir = data_dir + '/train'
+valid_dir = data_dir + '/valid'
+test_dir = data_dir + '/test'
+test_image_size = (224,224)
+checkpoint_path = "rabakClassifier2.pth"
